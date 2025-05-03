d85957e4a6d8cd47c2d10ef65207c5bce773162f
Injun Jeong <jijun897@naver.com>
2025-05-04 03:39:08 +0900

    feat: async/await 문법으로 변경하여 비동기 작업을 직관적으로 처리

--- api/tmdbApi.js
+++ api/tmdbApi.js
@@ -1,36 +1,37 @@
 import API_KEY from "./config.js";
 
-export default function Top5Movies() {
+export async function Top5Movies() {
   const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      const top5 = data.results.slice(0, 5);
-      return top5;
-    });
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Network response was not ok" + response.statusText);
+  }
+  const data = await response.json();
+
+  return data.results.slice(0, 5);
 }
 
-export default function searchMovies(query) {
-  const searchInput = document.getElementById("searchInput");
-  const query = searchInput.value; // 검색어
+export async function searchMovies(query) {
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
     query
   )}&language=ko-KR`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      return data.results;
-    });
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok" + response.statusText);
+  }
+  const data = await response.json();
+  return data.results;
 }
 
-export default function getMovieDetails(movieId) {
+export async function getMovieDetails(movieId) {
   const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=ko-KR`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    });
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Network response was not ok" + response.statusText);
+  }
+  const data = await response.json();
+  return data;
 }
