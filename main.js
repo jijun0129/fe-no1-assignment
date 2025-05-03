1c2b290d96d5c00a29f6f05e1d3a387a5c4effcd
Injun Jeong <jijun897@naver.com>
2025-05-04 03:39:42 +0900

    feat: 영화 검색, 모달을 통한 상세보기, 초기 top5 영화 보여주는 기능 추가

--- main.js
+++ main.js
@@ -1,1 +1,101 @@
+import { Top5Movies, searchMovies, getMovieDetails } from "./api/tmdbApi.js";
+
 const searchButton = document.getElementById("searchButton");
+
+searchButton.addEventListener("click", async () => {
+  const searchInput = document.getElementById("searchInput");
+  const query = searchInput.value;
+  const data = await searchMovies(query);
+  console.log(data);
+  const searchError = document.getElementById("searchError");
+  searchError.innerHTML = "";
+  const searchResults = document.getElementById("searchResults");
+  searchResults.innerHTML = ""; // 이전 검색 결과 초기화
+
+  if (data.length === 0) {
+    const searchError = document.getElementById("searchError");
+    searchError.innerHTML = "검색 결과가 없습니다.";
+    return;
+  }
+  data.forEach((movie, index) => {
+    const movieCard = document.createElement("div");
+    movieCard.className = "movieCard";
+    movieCard.id = movie.id;
+    movieCard.innerHTML = `
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+          <h3>${movie.title}</h3>
+          <p>평점: ${movie.vote_average}</p>
+          <p>${movie.overview}</p>
+        `;
+
+    searchResults.appendChild(movieCard);
+  });
+});
+
+const searchResults = document.getElementById("searchResults");
+searchResults.addEventListener("click", async (event) => {
+  const movieCard = event.target.closest(".movieCard");
+  if (movieCard) {
+    const movieId = movieCard.id;
+    const data = await getMovieDetails(movieId);
+    showModal(data);
+  }
+});
+
+function showModal(movie) {
+  const oldModal = document.getElementById("movieModal");
+  if (oldModal) oldModal.remove();
+
+  // 모달 요소 생성
+  const modal = document.createElement("div");
+  modal.id = "modal";
+
+  // 모달 내용
+  modal.innerHTML = `
+    <div id="movieModal">
+      <button id="closeModalBtn">&times;</button>
+      <div>
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" id="modalImage"">
+      </div>
+      <div class="modalText">
+        <h2 style="margin: 10px 0;">${movie.title}</h2>
+        <p><strong>장르:</strong> ${movie.genres.map((genre) => genre.name).join(", ")}</p>
+        <p><strong>개봉일:</strong> ${movie.release_date}</p>
+        <p><strong>상영시간:</strong> ${movie.runtime}분</p>
+        <p><strong>평점:</strong> ${movie.vote_average}</p>
+        <p style="margin-top: 16px;"><strong>줄거리: </strong>${movie.overview}</p>
+      </div>
+    </div>
+  `;
+
+  // 모달 닫기 버튼 이벤트
+  modal.querySelector("#closeModalBtn").addEventListener("click", () => {
+    modal.remove();
+  });
+
+  // 모달 바깥 클릭 시 닫기
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) modal.remove();
+  });
+
+  document.body.appendChild(modal);
+}
+
+const top5Movies = await Top5Movies();
+console.log(top5Movies);
+
+const searchTitle = document.getElementById("searchTitle");
+searchTitle.innerText = "현재 Top5 Movie"; // 이전 검색 결과 초기화
+top5Movies.forEach((movie, index) => {
+  const movieCard = document.createElement("div");
+  movieCard.className = "movieCard";
+  movieCard.id = movie.id;
+  movieCard.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+        <h3>${movie.title}</h3>
+        <p>평점: ${movie.vote_average}</p>
+        <p>${movie.overview}</p>
+      `;
+
+  searchResults.appendChild(movieCard);
+});
